a4e332adc285ec461d268d63e488e63c7065cafe
Ulvi <ulvi.sadiqov.09@mail.ru>
2021-01-31 18:29:47 +0400

    adding all file and finish

---
+++ date.js
@@ -0,0 +1,43 @@
+let h1 = document.getElementById("h");
+setTimeout( function(){
+ h1.innerText="december 2019" 
+},1000);
+setTimeout( function(){
+ h1.innerText="january 2020" 
+},2000);
+setTimeout( function(){
+ h1.innerText="february 2020" 
+},3000);
+setTimeout( function(){
+ h1.innerText="march 2020" 
+},4000);
+setTimeout(function(){
+ h1.innerText="april 2020" 
+},5000);
+setTimeout( function(){
+ h1.innerText="may 2020" 
+},6000);
+setTimeout( function(){
+ h1.innerText="june 2020" 
+},7000);
+setTimeout( function(){
+ h1.innerText="july 2020" 
+},8000);
+setTimeout( function(){
+ h1.innerText="august 2020" 
+},9000);
+setTimeout(10000, function(){
+ h1.innerText="september 2020" 
+},10000);
+setTimeout( function(){
+ h1.innerText="octember 2020" 
+},11000);
+setTimeout( function(){
+ h1.innerText="november 2020" 
+},12000);
+setTimeout( function(){
+ h1.innerText="december 2020" 
+},13000);
+setTimeout( function(){
+ h1.innerText="january 2021" 
+},14000);
